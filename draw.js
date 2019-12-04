8d63d45d87939a1498752d587fd5efec9c7d84f8
H <hannah.abigail.com@gmail.com>
2019-12-04 16:32:56 -0500

    rar

--- draw.js
+++ draw.js
@@ -1,8 +1,31 @@
 //draw.js
 
+let emptydiv = function(id) { return `<div id="${id}"></div>`; }
+
+let makenav = function() {
+  let navigation = $("#nav");
+  let navtext = "Random book display";
+  navigation.html(`
+    <div id="navheader">${navtext}</div>
+  `);
+}
+
+let makebod = function() {
+  let body = $("#body");
+  console.log(body);
+  body.html(`
+    dogs
+  `);
+}
+
 let start = function() {
-  $(".test")
-    .html("testing");
+  $("body")
+    .html(
+      emptydiv("nav") +
+      emptydiv("body")
+    );
+  makenav()
+  makebod()
 };
 
 $(start);
