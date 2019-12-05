f966a9305c6cadaaafde7cced1e84101d5fe73fc
H <hannah.abigail.com@gmail.com>
2019-12-05 17:59:12 -0500

    rar

--- record.py
+++ record.py
@@ -1,12 +1,26 @@
 import urllib, urllib.request
 import random
 from bs4 import BeautifulSoup
+import json
 
 RN_LOW = 1000001
 RN_HIGH = 3835994
 def randomurl():
   URL = "https://mercury.concordia.ca/search/?searchtype=.&searcharg=b%07d"
   return URL % random.randint(RN_LOW, RN_HIGH)
+
+def remove_some_styling(tag):
+  for z in tag.select("strong"):
+    z.unwrap()
+
+def one_child(entry):
+  names = entry.select(".bibInfoLabel")
+  count = len(names)
+  if count == 0:
+    return # this one is empty maybe?
+  if count > 1:
+    raise Exception("more than one bibInfoLabel")
+  name = names[0]
 
 def getrec():
   try:
@@ -17,8 +31,23 @@ def getrec():
       'reason': e.reason,
       'headers': e.headers
     }
-  soup = BeautifulSoup(req.read(), 'html.parser')
+  try:
+    soup = BeautifulSoup(req.read(), 'html.parser')
+    fiel = {}
+    for z in fiel.select(".bibInfoEntry"):
+      remove_some_styling(z)
+      name = get_name_from_entry(z)
+      name = name.string
+      datas = z.select(".bibInfoData")
+
+  except Exception as e:
+    return {
+      'code': 200,
+      'fields': fiel,
+      'error': repr(e)
+    }
   return {
     'code': req.getcode(),
-    'soup': soup
+    'fields': fiel,
+    'error': None
   }
