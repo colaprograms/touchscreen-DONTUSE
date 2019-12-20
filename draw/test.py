c1a5036de588cbf1bb02f28d6429e3cd08807434
H <hannah.abigail.com@gmail.com>
2019-12-20 13:58:45 -0500

    rar

--- draw/test.py
+++ draw/test.py
@@ -64,26 +64,42 @@ class facedetector:
 
         self.pipeline.start(self.config)
 
+        class pyramid:
+            def __init__(self):
+                pass
+
+            def __call__(self, im):
+                return im
+
+            def rect_down(self, rect):
+                return rect
+
+            def rect_up(self, rect):
+                return rect
+
+        self.scaledown = dlib.pyramid_down()
+
         self.alignment = rs.align(rs.stream.color)
 
     def face_detector(self, im):
+        scaled_image = self.scaledown(im)
         def usewholeimg():
             facebbtimer.start()
-            facebb = self.fd(im)
+            facebb = self.fd(scaled_image)
             facebbtimer.stop()
             if len(facebb) > 0:
                 return facebb[0]
             return None
 
         def usesmallbox(t, b, l, r, scale, scale_top):
-            h, w, _ = im.shape
+            h, w, _ = scaled_image.shape
             fh = b - t
             fw = r - l
             t = max(0, t - int(scale_top*fh))
             b = min(h, b + int(scale*fh))
             l = max(0, l - int(scale*fw))
             r = min(w, r + int(scale*fw))
-            facebb = self.fd(im[t:b, l:r, :])
+            facebb = self.fd(scaled_image[t:b, l:r, :])
             if len(facebb) >= 1:
                 if len(facebb) > 1:
                     print("multiple faces detected, choosing one arbitrarily")
@@ -97,7 +113,9 @@ class facedetector:
         else:
             shortftimer.start()
             t, b, l, r = decompose(self.currentbb)
-            rect = usesmallbox(t, b, l, r, 0.3, 0.1)
+            SCALE_TOP = 0.1
+            SCALE = 0.05
+            rect = usesmallbox(t, b, l, r, SCALE_TOP, SCALE)
             if not rect:
                 rect = usewholeimg()
             self.currentbb = rect
@@ -106,7 +124,7 @@ class facedetector:
             shapestimer.start()
             out = self.sp.calldpoint(
                 im,
-                self.currentbb
+                self.scaledown.rect_up(self.currentbb)
             )
             shapestimer.stop()
             return out
@@ -165,7 +183,7 @@ class dampve:
 
     def add(self, t, z):
         if t == self.t0:
-            return
+            return self.x0
         if self.x0 is None:
             self.x0 = z
         elif self.v0 is None:
@@ -181,18 +199,20 @@ class dampve:
 
 class predicteyes:
     def __init__(self):
-        self.t0 = None
-        # add a lot more damping on y
-        self.mids = [
-            dampve(1, 0.1),
-            dampve(1, 0.1),
-            dampve(1, 0.1)
-        ]
+        self.stop()
         self.out = np.zeros(3)
+
+    def stop(self):
+        self.t0 = None
 
     def __call__(self, t, x, y, z):
         if self.t0 is None:
             self.t0 = t
+            self.mids = [
+                dampve(1, 0.1),
+                dampve(1, 0.1),
+                dampve(1, 0.1)
+            ]
         t -= self.t0
         for (j, c) in enumerate([x, y, z]):
             self.out[j] = self.mids[j].add(t, c) #(c)
@@ -207,8 +227,10 @@ class predicteyes:
         print("t = %6.2f   scale = %6.2f   l = %9.2f %9.2f"% (t, scale, l[0], l[1]))
     """
 
+import traceback as tb
 PARTS_L_EYE = range(36, 42)
 PARTS_R_EYE = range(42, 48)
+PARTS_NOSE_TOP = 27
 def average_on_box_around_point(img, x, y, scale):
     x = int(x)
     y = int(y)
@@ -219,20 +241,34 @@ def average_on_box_around_point(img, x, y, scale):
     t = max(0, y - scale)
     b = min(img.shape[0], y + scale)
     img = img[t:b, l:r]
-    return np.mean(img[img != 0])
+    ret = np.mean(img[img != 0])
+    if not np.any(img != 0):
+        print("ret =", ret)
+    return ret
+
+def meanof(shape, ix):
+    sp = [shape.part(i) for i in ix]
+    v = np.array([[p.x, p.y] for p in sp])
+    return np.mean(v, axis=0), np.std(v, axis=0)
 
 def coordinates(t, depthimage, colorimage, shape):
     left, leftv = meanof(shape, PARTS_L_EYE)
     right, rightv = meanof(shape, PARTS_R_EYE)
+    nose, _ = meanof(shape, (PARTS_NOSE_TOP,))
     lscale = leftv[0]
     rscale = rightv[0]
     ld = average_on_box_around_point(depthimage, left[0], left[1], lscale)
     rd = average_on_box_around_point(depthimage, right[0], right[1], rscale)
-    return np.array([(left[0] + right[0]) / 2, (left[1] + right[1]) / 2, (ld + rd) / 2])
+    return np.array([
+        (left[0] + right[0] + nose[0]) / 3,
+        (left[1] + right[1] + nose[1]) / 3,
+        (ld + rd) / 2
+    ])
 
 class imagewindow:
     def __init__(self, act = True):
         self.wi = dlib.image_window() if act else None
+        self.framecount = 0
 
     def show(self, out):
         if not self.wi:
@@ -241,11 +277,22 @@ class imagewindow:
         t, depthimage, colorimage, shape = out
         wi.set_image(colorimage)
         wi.clear_overlay()
+        self.framecount += 1
         if shape is not None:
             def topoint(dp):
                 return dlib.point(dp.x, dp.y)
-            fobp = dlib.full_object_detection(shape.rect, [topoint(shape.part(i)) for i in range(shape.num_parts)])
-            #wi.add_overlay(fobp)
+            if False:
+                fobp = dlib.full_object_detection(shape.rect, [topoint(shape.part(i)) for i in range(shape.num_parts)])
+                for jj in range(fobp.num_parts):
+                    p = fobp.part(jj)
+                    if jj > self.framecount % 68:
+                        break
+                    self.wi.add_overlay_circle(
+                        dlib.dpoint(p.x, p.y),
+                        2,
+                        color = dlib.rgb_pixel(255, 255, 0)
+                    )
+                wi.add_overlay(fobp)
             #wi.add_overlay(shape.rect)
 
     def overlay_circle(self, x, y):
@@ -258,11 +305,7 @@ if __name__ == "__main__":
     fd.start()
     framecount = -1
     predict = predicteyes()
-    iw = imagewindow()
-    def meanof(shape, ix):
-        sp = [shape.part(i) for i in ix]
-        v = np.array([[p.x, p.y] for p in sp])
-        return np.mean(v, axis=0), np.std(v, axis=0)
+    #iw = imagewindow()
     try:
         while True:
             otherstimer.stop()
@@ -273,27 +316,35 @@ if __name__ == "__main__":
                 if framecount > 0:
                     if framecount % 100 == 0:
                         print(f"FPS: {framecount / (time.time() - starttime)}")
-                        """
-                        print(f"Face bounding boxes take {facebbtimer.get()} on average")
-                        print(f"Shape prediction takes {shapestimer.get()} on average")
-                        print(f"Small face bounding boxes: {shortftimer.get()} on average")
-                        print(f"Other stuff: {otherstimer.get()} on average")
-                        """
+                        #print(f"Face bounding boxes take {facebbtimer.get()} on average")
+                        #print(f"Shape prediction takes {shapestimer.get()} on average")
+                        #print(f"Small face bounding boxes: {shortftimer.get()} on average")
+                        #print(f"Other stuff: {otherstimer.get()} on average")
                         framecount = 0
                         starttime = time.time()
                 else:
                     starttime = time.time()
                 t, depthimage, colorimage, shape = out
+                foundeyes = False
                 if shape is not None:
                     (x, y, z) = coordinates(t, depthimage, colorimage, shape)
-                    t, [x, y, z] = predict(t, x, y, z)
-                    print("%6.2f    %6.2f    %6.2f" % (x, y, z))
-                    if framecount & 0 == 0:
-                        iw.show(out)
-                        iw.overlay_circle(x, y)
-                    #print(t, x, y, z)
-                    #predict(t, left, right)
-                else:
+                    if np.any(np.isnan([x, y, z])):
+                        pass
+                    else:
+                        foundeyes = True
+                        #iw.show(out)
+                        #iw.overlay_circle(x, y)
+                        t, [x, y, z] = predict(t, x, y, z)
+                        z /= 1000
+                        SCREEN_HALF_WIDTH = 0.27
+                        CAMERA_HALF_WIDTH = 1.60
+                        SCALE = SCREEN_HALF_WIDTH / CAMERA_HALF_WIDTH
+                        x = (x - 320) * z / 100 * SCALE
+                        y = (y - 240) * z / 100 * SCALE
+                        print("%6.2f    %6.2f    %6.2f" % (x, y, z))
+                        #iw.overlay_circle(x * 100 + 320, y * 100 + 240)
+                if not foundeyes:
+                    predict.stop()
                     print("no eyes found this frame")
     finally:
         fd.stop()
